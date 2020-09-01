7b03a9ca8d079724cac5ef1aef49b50cf6e77c50
J. Trpka <jtrpka0912@gmail.com>
2020-09-01 08:29:29 -0400

    Added a 'simple' header and footer component for layout.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,10 +2,14 @@
 import React from 'react';
 
 // Layout
+import Page from '../layout/page/Page';
 
 const App = () => {
   return (
     <div className="App">
+      <Page>
+
+      </Page>
     </div>
   );
 }
--- src/components/layout/page/Page.tsx
+++ src/components/layout/page/Page.tsx
@@ -1,11 +1,40 @@
 // React
 import React from 'react';
 
+/**
+ * @function PageHeader
+ * @summary Page Header Layout
+ * @description The header for all pages for the web app
+ * @author J. Trpka <jtrpka0912@gmail.com>
+ * @returns { JSX }
+ */
+const PageHeader: React.FunctionComponent = () => {
+    return (
+        <header className="shelf-page-header">
+            <p>This is the header</p>
+        </header>
+    );
+};
+
+/**
+ * @function PageFooter
+ * @summary Page Footer Layout
+ * @description The footer for all pages for the web app
+ * @author J. Trpka <jtrpka0912@gmail.com>
+ * @returns { JSX }
+ */
+const PageFooter: React.FunctionComponent = () => {
+    return (
+        <footer className="shelf-page-footer">
+            <p>This is the footer</p>
+        </footer>
+    );
+};
 
 /**
  * @function Page
- * @summary Main page layout
- * @description The layout for all pages for this web app
+ * @summary Page Layout Container
+ * @description The layout for all pages for the web app
  * @author J. Trpka <jtrpka0912@gmail.com>
  * @param { object } props 
  * @returns { JSX }
@@ -13,7 +42,9 @@ import React from 'react';
 const Page: React.FunctionComponent = (props) => {
     return (
         <main className="shelf-page-wrapper">
+            <PageHeader />
             { props.children };
+            <PageFooter />
         </main>
     );
 };
