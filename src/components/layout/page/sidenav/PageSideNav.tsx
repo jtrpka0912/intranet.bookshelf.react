bdeca87966427fe911dbdc773ca8f0a7750c8960
J. Trpka <jtrpka0912@gmail.com>
2020-09-27 10:11:16 -0400

    Restructured the shelf list items.

--- src/components/layout/page/sidenav/PageSideNav.tsx
+++ src/components/layout/page/sidenav/PageSideNav.tsx
@@ -19,19 +19,31 @@ import { ShelfContext } from '../../../../contexts/ShelfContext';
  */
 const PageSideNav: React.FunctionComponent = () => {
     // Context
-    const { shelves } = useContext(ShelfContext);
+    const { shelves, activeShelf } = useContext(ShelfContext);
     
     return (
         <aside className="shelf-page-sidebar opened">
             <section className="shelf-page-sidebar-shelves">
                 <h3>Shelves</h3>
 
-                <ul className="no-bullets">
+                <ul className="shelf-shelves-listing no-bullets">
                     { shelves.map((item) => {
+                        // NOTE: This might be in a separate component. Not sure...
                         // TODO: Need to show a marker that this shelf is the current shelf
                         // TODO: Need to add onClick events to change current shelf
-                        // TODO: Need to add markers to show that it shows directories and/or multi-file
-                        return <li key={item._id}>{item.name}</li>  
+                        return (
+                            <li key={item._id} className={`shelves-item ${ activeShelf?._id === item._id ? 'active' : '' }`}>
+                                <div className="shelves-item-name">
+                                    {item.name}
+                                </div>
+                                
+                                <div className="shelves-item-icons">
+                                    { item.multiFile ? <span className="shelves-item-icons-multifile">m</span> : null }
+                                    { item.showDirectories ? <span className="shelves-item-icons-showdirectories">d</span> : null }
+                                    <span className="shelves-item-icons-info">i</span>
+                                </div>
+                            </li>  
+                        ); 
                     }) }
                 </ul>
             </section>
