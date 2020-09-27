9ff5ba56043d01904df60363b9168cc82c99a62f
J. Trpka <jtrpka0912@gmail.com>
2020-09-27 09:42:43 -0400

    Removed app context, for now, to reduce warnings.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,19 +5,16 @@ import React from 'react';
 import PageWrapper from '../layout/page/wrapper/PageWrapper';
 
 // Contexts
-import AppContextProvider from '../../contexts/AppContext';
 import ShelfContextProvider from '../../contexts/ShelfContext';
 
 const App = () => {
 
     return (
-        <AppContextProvider>
-            <ShelfContextProvider>
-                <PageWrapper>
-                    <p>This is page content</p>
-                </PageWrapper>
-            </ShelfContextProvider>
-        </AppContextProvider>
+        <ShelfContextProvider>
+            <PageWrapper>
+                <p>This is page content</p>
+            </PageWrapper>
+        </ShelfContextProvider>
     );
 }
 
