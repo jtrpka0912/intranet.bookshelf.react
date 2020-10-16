bf1255aaa011c5a8f7597ee9ca158d5268eabc4f
J. Trpka <jtrpka0912@gmail.com>
2020-10-16 08:20:56 -0400

    Added state for the list displays in the app context.

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -2,22 +2,53 @@
 import React, { createContext, useState } from 'react';
 
 /**
+ * @exports
+ * @enum ListViews
+ * @description The defined view types for directories, breadcrumbs, and the files components
+ * @author J.T.
+ * @property { string } Tile
+ * @property { string } List
+ * @property { string } Breadcrumb
+ */
+export enum ListViews {
+    Tile = 'tile',
+    List = 'list',
+    Breadcrumb = 'breadcrumb'
+};
+
+/**
  * @type AppContextType
  * @summary Application Context Type
  * @description Definition of the application context type
  * @author J.T.
  * @property { boolean } isSideNavOpen
+ * @property { ListView } breadcrumbView
+ * @property { ListView } directoryView
+ * @property { ListView } fileView
+ * @property { function } toggleSideNav
  */
 type AppContextType = {
     // State
     isSideNavOpen: boolean,
+    breadcrumbView: ListViews,
+    directoryView: ListViews,
+    fileView: ListViews
     // Actions
-    toggleSideNav: (state: boolean) => void
+    toggleSideNav: (state: boolean) => void,
+    toggleBreadcrumbView: (state: ListViews) => void,
+    toggleDirectoryView: (state: ListViews) => void,
+    toggleFileView: (state: ListViews) => void
 };
 
 const defaultState: AppContextType = {
     isSideNavOpen: false,
-    toggleSideNav: toggleSideNav => console.warn('toggleSideNav is not available (check context provider in heirarchy)')
+    breadcrumbView: ListViews.Tile,
+    directoryView: ListViews.Tile,
+    fileView: ListViews.Tile,
+    toggleSideNav: toggleSideNav => console.warn('toggleSideNav is not available (check context provider in heirarchy)'),
+    toggleBreadcrumbView: toggleSideNav => console.warn('toggleBreadcrumbView is not available (check context provider in heirarchy)'),
+    toggleDirectoryView: toggleSideNav => console.warn('toggleDirectoryView is not available (check context provider in heirarchy)'),
+    toggleFileView: toggleSideNav => console.warn('toggleFileView is not available (check context provider in heirarchy)')
 };
 
 export const AppContext: React.Context<AppContextType> = createContext<AppContextType>(defaultState);
@@ -45,11 +76,20 @@ const AppContextProvider = (props: AppContextProps) => {
     // States
     // TODO: Need to figure out how to toggle when clicking ANYWHERE outside of side navigation.
     const [isSideNavOpen, toggleSideNav] = useState(defaultState.isSideNavOpen);
+    const [breadcrumbView, toggleBreadcrumbView] = useState(defaultState.breadcrumbView);
+    const [directoryView, toggleDirectoryView] = useState(defaultState.directoryView);
+    const [fileView, toggleFileView] = useState(defaultState.fileView);
 
     return (
         <AppContext.Provider value={{
             isSideNavOpen,
-            toggleSideNav
+            breadcrumbView,
+            directoryView,
+            fileView,
+            toggleSideNav,
+            toggleBreadcrumbView,
+            toggleDirectoryView,
+            toggleFileView
         }}>
             { props.children }
         </AppContext.Provider>
