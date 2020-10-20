f4d93ec781b51d6b84e17208c81f4be398667653
J. Trpka <jtrpka0912@gmail.com>
2020-10-20 05:53:06 -0400

    Built the starting structure of a text field.

---
+++ src/components/common/textfield/TextField.tsx
@@ -0,0 +1,15 @@
+// React
+import React from 'react';
+
+// Styles
+import './TextField.scss';
+
+const TextField: React.FunctionComponent = () => {
+    return (
+        <label>
+            <input type="text" />
+        </label>
+    );
+};
+
+export default TextField;
