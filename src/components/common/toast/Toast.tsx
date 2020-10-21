b6d40c4b44b383dcc23cb0dcd5c34f1737294707
J. Trpka <jtrpka0912@gmail.com>
2020-10-21 07:19:51 -0400

    Created the basic structure of the toast component.

---
+++ src/components/common/toast/Toast.tsx
@@ -0,0 +1,20 @@
+// React
+import React from 'react';
+
+// Styles
+import './Toast.scss';
+
+/**
+ * @function Toast
+ * @summary Toast Component
+ * @description A toast message popup to alert messages to the user.
+ * @author J.T.
+ * @returns { React.ReactNode }
+ */
+const Toast: React.FunctionComponent = () => {
+    return (
+        <div className="common-toast"></div>
+    );
+};
+
+export default Toast;
