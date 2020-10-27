d1d08d631e0f84afccba66d8bf4cc61e4297a40d
J. Trpka <jtrpka0912@gmail.com>
2020-10-27 08:08:35 -0400

    Added refresh shelf functionality.

--- src/components/project/shelf-modal/ShelfModal.tsx
+++ src/components/project/shelf-modal/ShelfModal.tsx
@@ -5,6 +5,7 @@ import React, { useContext } from 'react';
 import { ShelfContext } from '../../contexts/ShelfContext';
 
 // Components
+import Button from '../../common/button/Button';
 import Modal from '../../common/modal/Modal';
 import ShelfForm from '../../project/shelf-form/ShelfForm';
 
@@ -16,14 +17,41 @@ import ShelfForm from '../../project/shelf-form/ShelfForm';
  * @returns { React.ReactNode }
  */
 const ShelfModal: React.FunctionComponent = () => {
-    const { activeShelf } = useContext(ShelfContext);
+    const { activeShelf, setToActiveShelf } = useContext(ShelfContext);
+
+    /**
+     * @function renderModalFooter
+     * @description Render the modal footer
+     * @author J.T.
+     * @returns { React.ReactNode }
+     */
+    const renderModalFooter = () => {
+        /**
+         * @async
+         * @function onClickRefreshShelf
+         * @description Refresh the shelf to (de)populate items.
+         * @author J.T.
+         */
+        const onClickRefreshShelf = async () => {
+            if(activeShelf) {
+                await fetch(`http://localhost:3001/api/v1/shelves/${ activeShelf._id }/refresh`);
+                setToActiveShelf(activeShelf);
+            }
+        };
+
+        return (
+            <React.Fragment>
+                <Button onClick={ () => onClickRefreshShelf() }>Refresh Shelf</Button>
+            </React.Fragment>
+        );
+    }
 
     // TODO: Close the modal after form submission
-
     if(activeShelf) {
         return (
             <Modal open={ true }
                 title={ `Edit ${ activeShelf.name }` }
+                footer={ renderModalFooter() }
                 size="small"
                 fade
                 onClose={ () => console.log('Goodbye form') }
